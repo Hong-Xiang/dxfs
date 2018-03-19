a8021cea5aa5ebd55b0963858082fba5609a46ed
HongXiang <hx.hongxiang@gmail.com>
2018-03-19 19:50:34 +0800

    add fs depens

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='dxl-fs',
-      version='0.1.2',
+      version='0.1.4',
       description='File system library.',
       url='https://github.com/Hong-Xiang/dxl',
       author='Hong Xiang',
@@ -11,6 +11,7 @@ setup(name='dxl-fs',
       install_requires=[
           'click',
           'rx',
+          'fs',
       ],
       scripts=['src/cli/dxfs.py'],
     #   namespace_packages = ['dxl'],
